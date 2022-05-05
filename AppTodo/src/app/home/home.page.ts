755e62bc263689e026590d731b901448297f64c8
KewryMFranck <100322668+KewryMFranck@users.noreply.github.com>
2022-05-04 23:36:14 -0300

    https://github.com/KewryMFranck/AppTodo.git

--- AppTodo/src/app/home/home.page.ts
+++ AppTodo/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AlertController } from '@ionic/angular';
+import { ActionSheetController, AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +8,10 @@ import { AlertController } from '@ionic/angular';
 })
 export class HomePage {
 
-  constructor(private alertCrtl: AlertController) {}
+//
+tarefas: any[] = [];
+ 
+  constructor(private alertCrtl: AlertController, private toastCtrl: ToastController,private actionSheetCrtl: ActionSheetController)   {}
 
      async showAdd(){
        const alert = await this.alertCrtl.create({
@@ -32,12 +35,74 @@ export class HomePage {
             },
             {
               text: 'Adicionar',
-              handler:()=>{
-                console.log('cancelado com sucesso!'); 
+              handler:(form) => {
+                this.adicionaTarefa(form.tarefa1); 
+            
               },
             },
           ],
        });
-        await alert.present();
+
+       await alert.present();
+
 }
+//
+async adicionaTarefa(novaTarefa: string){
+  if(novaTarefa.trim().length < 1) {
+    const toast = await this.toastCtrl.create({
+      message: 'Por favor, digite a tarefa!',
+      duration: 2000,
+      position: 'top',
+      
+    });
+toast.present();
+return;
+  }
+
+  //
+const tarefa = { nome:novaTarefa, realizada: false };
+//
+this.tarefas.push(tarefa);
+this.salvaLocalStorage();
+
 }
+salvaLocalStorage(){
+  localStorage.setItem('tarefaUsuario', JSON.stringify(this.tarefas));
+}
+async realizaAcoes(tarefa: any){
+  const actionSheet = await this.actionSheetCrtl.create({
+  header: 'Qual ação realizar?',
+  buttons: [{
+    text: tarefa.realizada ? ' Desmarcar' : 'Marcar',
+    icon: tarefa.realizada ? 'checkmar-circle' : 'radio-button-off-outline',
+    handler: () => {
+      tarefa.realizada = ! tarefa.realizada;
+      this.salvaLocalStorage();
+      
+    
+    let tarefaSalva = localStorage.getItem( 'tarefaUsuario');
+
+    if (tarefaSalva != null){
+      this.tarefas = JSON.parse(tarefaSalva);}
+    
+  }
+  excluirTarefa( tarefa: any){
+    this.tarefas = this.tarefas.filter(arrayTarefa => != arrayTarefa);
+    
+    this.salvaLocalStorage();
+  }
+},
+  {
+    text: 'cancelar',
+    icon: ' close',
+    role: 'cancel',
+    handler: () => {
+      console.log('Cancel clicked');
+    }
+  }]
+  }); 
+  await actionSheet.present();
+  const { role,data } = await actionSheet.onDidDismiss();
+    }
+  }
+ 
